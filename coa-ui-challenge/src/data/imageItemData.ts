16b752a88c94cd71bf9f11beb6192f5a9bb0ab95
Optimus ged <gedeonyalakuhanda@example.com>
2024-06-08 00:10:16 +0300

    imageItemData list created

---
+++ coa-ui-challenge/src/data/imageItemData.ts
@@ -0,0 +1,31 @@
+import image1 from "./assets/image1.png";
+import image2 from "./assets/image2.png";
+import image3 from "./assets/image3.png";
+import image4 from "./assets/image4.png";
+
+export const imageItemData = [
+  {
+    url: image1,
+    title: "FENNEC",
+    subTitle: "FOX",
+    location: "India",
+  },
+  {
+    url: image2,
+    title: "HUMPBACK",
+    subTitle: "WHALE",
+    location: "South Africa",
+  },
+  {
+    url: image3,
+    title: "COMMON BROWN",
+    subTitle: "BABOON",
+    location: "South Africa",
+  },
+  {
+    url: image4,
+    title: "SPOTTED",
+    subTitle: "DEER",
+    location: "Amazon",
+  },
+];
