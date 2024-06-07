01edfb56148219b4a6d0aa5958ba3385393b08e2
Optimus ged <gedeonyalakuhanda@example.com>
2024-06-07 23:51:52 +0300

    first image imported and displayed

--- coa-ui-challenge/src/components/ImageItem.tsx
+++ coa-ui-challenge/src/components/ImageItem.tsx
@@ -1,7 +1,11 @@
-export default function ImageItem(){
-    return <div className="overflow-hidden relative h-[588px] w-[98.5px] sm:w-[283px] group">
-        <div className="h-[588px] w-[283px] group-hover:scale-105 group-hover:grayscale transition duration-[0.4s] group-hover:blur-[5px] translate-x-[-34%] lg:translate-x-0 relative">
-            
-        </div>
+import image1 from "../assets/image1.png";
+
+export default function ImageItem() {
+  return (
+    <div className="overflow-hidden relative h-[588px] w-[98.5px] sm:w-[283px] group">
+      <div className="h-[588px] w-[283px] group-hover:scale-105 group-hover:grayscale transition duration-[0.4s] group-hover:blur-[5px] translate-x-[-34%] lg:translate-x-0 relative">
+        <img className="h-full w-full" src={image1} alt="" />
+      </div>
     </div>
+  );
 }
