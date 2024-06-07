bcd8f1f0f639b27c40d41d8bf70e8c47c3a1b7cc
Optimus ged <gedeonyalakuhanda@example.com>
2024-06-07 23:49:50 +0300

    div where the hover effects will go added

--- coa-ui-challenge/src/components/ImageItem.tsx
+++ coa-ui-challenge/src/components/ImageItem.tsx
@@ -1,5 +1,7 @@
 export default function ImageItem(){
     return <div className="overflow-hidden relative h-[588px] w-[98.5px] sm:w-[283px] group">
-
+        <div className="h-[588px] w-[283px] group-hover:scale-105 group-hover:grayscale transition duration-[0.4s] group-hover:blur-[5px] translate-x-[-34%] lg:translate-x-0 relative">
+            
+        </div>
     </div>
 }
