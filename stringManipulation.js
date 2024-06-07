657ecae1d35309ecbccc9568e50dc0fcaef108ec
Optimus ged <gedeonyalakuhanda@example.com>
2024-06-07 18:30:27 +0300

    asiiConverter function created and tested successfully

--- stringManipulation.js
+++ stringManipulation.js
@@ -12,3 +12,19 @@ const reverse = (str) => {
 
 // let's test if it works
 // console.log(reverse("metu"))
+
+// let's now create the assii converter function for our current challenge
+const asiiConverter = (str) => {
+  // let's make sure the value passed is really a string
+  if (typeof str === "string") {
+    // let's create a value that will be receiving the converted values
+    let converted = "";
+
+    // we loop in the string to get the asii code of each character
+    for (let i in str) converted = `${converted} ${str.charCodeAt(i)}`;
+    return converted;
+  }
+};
+
+// let's check if it works as expected
+// console.log(asiiConverter("pzza"))
