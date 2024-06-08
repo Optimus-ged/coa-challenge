b8b88ac17629706b46ad3495743e023391b352f1
Optimus ged <gedeonyalakuhanda@example.com>
2024-06-08 13:44:50 +0300

    chevron left and chevron right icons added

--- coa-ui-challenge/src/components/customModal.tsx
+++ coa-ui-challenge/src/components/customModal.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type CustomModalType = {
   isOpened: boolean;
@@ -19,16 +20,21 @@ export default function CustomModal({
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.2 }}
       className="fixed inset-0 flex"
-      onClick={onClick}
     >
       <div className="bg-black h-full w-full relative">
         <img className="h-full m-auto" src={url} alt="" />
         <p
-          className="text-white absolute top-5 right-10 cursor-pointer hover:rounded-full hover:bg-neutral-700 h-10 w-10 flex items-center justify-center"
+          className="text-white absolute top-2 left-[50%] translate-x-[-50%] cursor-pointer rounded-full bg-neutral-700/30 h-10 w-10 flex items-center justify-center"
           onClick={onClick}
         >
           X
         </p>
+        <div className="h-10 w-10 hover:bg-neutral-700 rounded-full absolute flex top-[50%] translate-y-[-50%] cursor-pointer">
+          <ChevronLeft color="white" className="m-auto" />
+        </div>
+        <div className="right-0 bottom-[50%] translate-y-[50%] absolute h-10 w-10 hover:bg-neutral-700 rounded-full cursor-pointer flex ">
+          <ChevronRight color="white" className="m-auto" />
+        </div>
       </div>
     </motion.div>
   );
